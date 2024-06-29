a4d350c973ac1a31b75c026442a37c8edae1c1b4
nayanghoghari <ghogharinayan12@gmail.com>
2024-06-29 10:21:16 +0530

    store hash password using seeder

--- src/seeders/20240629033313-insert-default-users.js
+++ src/seeders/20240629033313-insert-default-users.js
@@ -1,14 +1,17 @@
 "use strict";
 
+const { hash } = require('bcrypt');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const hashPass = await hash("1234", 10)
     await queryInterface.bulkInsert("users", [
       {
         first_name: "admin",
         last_name: "user",
         email: "admin-user@gmail.com",
-        password: "1234",
+        password: hashPass,
         contact_number: "7777777777",
         post_code: "394107",
         hobbies: "bike-riding",
@@ -18,7 +21,7 @@ module.exports = {
         first_name: "root",
         last_name: "user",
         email: "root-user@gmail.com",
-        password: "1234",
+        password: hashPass,
         contact_number: "7777777777",
         post_code: "394107",
         hobbies: "video-games",
